24b3e0c5c4a6d2fb8eec4e832bfd27b8584ac33d
matthewngid20 <matthew.ng.id20@gmail.com>
2021-10-06 11:55:24 +1100

    Added list item object and render method

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect}from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const Item = (props) => {
+    return (
+      <View> 
+        <Text> {props.text}</Text>
+      </View>
+    )
+  }
+
+  const AppData = [
+    {id :1, name: "Apple"},
+    {
+      id : 2, name: "Orange"
+    },
+    {
+      id : 3, name: "Avocado"
+    },
+    {
+      id : 4, name: "Pineapple"
+    },
+    {
+      id : 5, name: "Banana"
+    }
+  ]
+
+  const Renderer = ({item}) => {
+    return (
+      <Item text = {item.name}/>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text></Text>
+      <FlatList
+      data = {AppData}
+      keyExtractor = { (item) => item.id.toString()}
+      renderItem= {Renderer}
+      />
+      
     </View>
   );
 }
@@ -15,7 +50,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
